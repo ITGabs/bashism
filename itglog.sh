eab1b8fdd1e5d98cd8da4cb62aed808808fec3d2
Gabriel ITGabs <itgabsns@gmail.com>
2014-10-13 21:16:42 +0700

    Update itglog.sh

--- itglog.sh
+++ itglog.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
 
-# need geoiplookup!
+# will show a colorfull real time log of everything related to apache cpanel
+
+# need grc!
 
 # instructions
 # copy and paste in vi vim or nano, give exec and run it
 #
-# vim net.sh
-# chmod 700 net.sh
-# ./net.sh
+# vim itglog.sh
+# chmod 700 itglog.sh
+# ./itglog.sh
 
-# Output
+# output
 
-# Hits 1 89.247.162.195 Germany
-# Hits 2 151.60.205.170 Italy
-# Hits 2 178.255.215.87 France
-# Hits 3 108.170.13.6 United
-# Hits 5 223.206.243.25 Thailand
-# Hits 18 190.63.172.226 Ecuador
+# 223.206.243.25 - - [13/Oct/2014:04:09:13 -0700] "GET /favicon.ico HTTP/1.1" 200 1995 "-" "Mozilla/5.0 (                                                                 Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.2.2000 Chrome/30.0.1599.101 S                                                                 afari/537.36"
+# 50.2.223.207 - - [13/Oct/2014:04:48:10 -0700] "POST 1.1" 200 1994 "http://bot.cl" "-"
+# 69.172.210.53 - - [13/Oct/2014:05:14:41 -0700] "GET HTTP/1.1 HTTP/1.1" 200 2016 "-" "Mozilla/4.0 (compa                                                                 tible; MSIE 6.0; Windows 98)"
+# ...
 
-
-for ip in `netstat -ntu | grep -v 'e' | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -n`;
+for f in `find /usr/local/apache/domlogs/ -maxdepth 1 -type f -mtime -30 | grep -v ftp | grep -v 'bytes_log' | grep -v 'ssl_log'`;
 do 
-  if [[ ${#ip} -gt "5" ]];
-    then 
-    if [[ $n -gt "0" ]]; 
-      then 
-      country=`geoiplookup $ip | head -n 1 | cut -d ' ' -f 5`;
-      city=`geoiplookup $ip | head -n 2 | tail -n 1 | cut -d ' ' -f 6,7,8,9`;
-      more=`#host $ip | cut -d ' ' -f 5`;
-      echo -e "Hits $n $ip $country "; 
-    fi; 
-  fi; 
-  n=$ip; 
+  list="$list $f";
 done;
+grc --config='conf.log' tail $list -f
