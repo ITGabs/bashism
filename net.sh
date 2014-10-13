2eca08747d07ebf5ef7ad13bdfa6ee5ba09838fc
Gabriel ITGabs <itgabsns@gmail.com>
2014-10-13 17:24:40 +0700

    Update net.sh
    Add some instructions and a example output

--- net.sh
+++ net.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# need geoiplookup!
+
+# instructions
+# copy and paste in vi vim or nano, give exec and run it
+#
+# vim net.sh
+# chmod 700 net.sh
+# ./net.sh
+
+# Output
+
+# Hits 1 89.247.162.195 Germany
+# Hits 2 151.60.205.170 Italy
+# Hits 2 178.255.215.87 France
+# Hits 3 108.170.13.6 United
+# Hits 5 223.206.243.25 Thailand
+# Hits 18 190.63.172.226 Ecuador
+
+
 for ip in `netstat -ntu | grep -v 'e' | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -n`;
 do 
   if [[ ${#ip} -gt "5" ]];
